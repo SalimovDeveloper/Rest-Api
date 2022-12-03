6951791fc2b7050e23d458c2c6ae1a61b4d1cfd6
salimovdeveloper <salimovdeveloper@gmail.com>
2022-12-03 12:36:44 -0600

    commit 2

---
+++ app/src/main/java/uz/salimovdeveloper/restapi/models/MyTodo.kt
@@ -0,0 +1,9 @@
+package uz.salimovdeveloper.restapi.models
+
+data class MyTodo(
+    val holat: String,
+    val id: Int,
+    val matn: String,
+    val oxirgi_muddat: String,
+    val sarlavha: String
+)
